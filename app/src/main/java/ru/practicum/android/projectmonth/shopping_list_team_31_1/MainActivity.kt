077a146e4e77db6843c1083c52f4d5c879e51d2b
Dmitry <dmitr.ter@gmail.com>
2026-05-26 01:57:30 +0300

    Цветовые константы, иконка приложения

--- app/src/main/java/ru/practicum/android/projectmonth/shopping_list_team_31_1/MainActivity.kt
+++ app/src/main/java/ru/practicum/android/projectmonth/shopping_list_team_31_1/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import ru.practicum.android.projectmonth.shopping_list_team_31_1.ui.theme.Shoppinglistteam311Theme
+import ru.practicum.android.projectmonth.shopping_list_team_31_1.ui.theme.ShoppingListTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Shoppinglistteam311Theme {
+            ShoppingListTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    Shoppinglistteam311Theme {
+    ShoppingListTheme {
         Greeting("Android")
     }
 }
--- app/src/main/java/ru/practicum/android/projectmonth/shopping_list_team_31_1/ui/theme/Color.kt
+++ app/src/main/java/ru/practicum/android/projectmonth/shopping_list_team_31_1/ui/theme/Color.kt
@@ -2,10 +2,13 @@ package ru.practicum.android.projectmonth.shopping_list_team_31_1.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val DarkText = Color(0xFF211A14)
+val LightBackground = Color(0xFFFFF8F4)
+val LightBrownSurface = Color(0xFFF4E6DA)
+val LightBrownElements = Color(0xFFFFDCBB)
+val RegularBrown = Color(0xFF845416)
+
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
