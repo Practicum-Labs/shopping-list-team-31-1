4b7037d4a778a02c6f1f40fa642c64c44f138ab3
abtomex <dchechumaev@mail.ru>
2026-05-25 15:49:54 +0300

    Создан проект

---
+++ app/src/main/java/ru/practicum/android/projectmonth/shopping_list_team_31_1/MainActivity.kt
@@ -0,0 +1,47 @@
+package ru.practicum.android.projectmonth.shopping_list_team_31_1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ru.practicum.android.projectmonth.shopping_list_team_31_1.ui.theme.Shoppinglistteam311Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Shoppinglistteam311Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Shoppinglistteam311Theme {
+        Greeting("Android")
+    }
+}
