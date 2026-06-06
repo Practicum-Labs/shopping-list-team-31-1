854c5261f54e1afa481a0e1e0a3d48360cfe8a18
dmitriy <dchechumaev@mail.ru>
2026-06-06 13:49:39 +0300

    rollback auth in main screen

--- app/src/main/java/ru/practicum/android/projectmonth/shoppinglist/ui/screens/MainScreen.kt
+++ app/src/main/java/ru/practicum/android/projectmonth/shoppinglist/ui/screens/MainScreen.kt
@@ -1,6 +1,5 @@
 package ru.practicum.android.projectmonth.shoppinglist.ui.screens
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,36 +8,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Icon
-import androidx.compose.material3.SecondaryTabRow
-import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRowDefaults
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import kotlinx.coroutines.launch
 import ru.practicum.android.projectmonth.shoppinglist.R
 import ru.practicum.android.projectmonth.shoppinglist.core.navigation.Destination
 import ru.practicum.android.projectmonth.shoppinglist.ui.components.IllustratedMessage
 import ru.practicum.android.projectmonth.shoppinglist.ui.theme.DarkText
-import ru.practicum.android.projectmonth.shoppinglist.ui.theme.LightBackground
 
-
-val YsDisplayRegular = FontFamily(
-    Font(R.font.ys_display_regular)
-)
 @Composable
 fun MainScreen(
     navController: NavController
@@ -46,86 +28,20 @@ fun MainScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .clickable {
+                navController.navigate(Destination.ShoppingLists.route)
+            }
     ) {
         Spacer(modifier = Modifier.height(16.dp))
 
         MainScreenTitle()
 
-        AuthAndRegisterViewPager(navController)
-
+        IllustratedMessage(
+            imageResId = R.drawable.img_main_screen,
+            headerResId = R.string.main_screen_header,
+            messageResId = R.string.main_screen_message
+        )
     }
-}
-
-@Composable
-fun AuthAndRegisterViewPager(navController: NavController) {
-    val pagerState = rememberPagerState(initialPage = 0) { 2 }
-    val coroutineScope = rememberCoroutineScope()
-
-    SecondaryTabRow(
-        selectedTabIndex = pagerState.currentPage,
-        contentColor = DarkText,
-        indicator = {
-            TabRowDefaults.SecondaryIndicator(
-                modifier = Modifier.tabIndicatorOffset(pagerState.currentPage),
-                height = 2.dp,
-                color = DarkText
-            )
-        }
-    ) {
-        listOf(
-            stringResource(R.string.login),
-            stringResource(R.string.register)
-        ).forEachIndexed { index, title ->
-            Tab(
-                modifier = Modifier.background(LightBackground),
-                selected = pagerState.currentPage == index,
-                onClick = {
-                    coroutineScope.launch {
-                        pagerState.animateScrollToPage(index)
-                    }
-                },
-                text = {
-                    Text(
-                        text = title,
-                        color = if (pagerState.currentPage == index)
-                            DarkText
-                        else
-                            DarkText.copy(alpha = 0.7f),
-                        fontFamily = YsDisplayRegular,
-                        fontSize = 14.sp
-                    )
-                }
-            )
-        }
-    }
-    HorizontalPager(
-        state = pagerState,
-        modifier = Modifier.fillMaxSize()
-    ) { page ->
-        when (page) {
-            0 -> LoginScreen(navController)      // Создайте этот компонент
-            1 -> RegisterScreen()   // Создайте этот компонент
-        }
-    }
-}
-
-@Composable
-fun LoginScreen(navController: NavController) {
-    IllustratedMessage(
-        imageResId = R.drawable.img_main_screen,
-        headerResId = R.string.main_screen_header,
-        messageResId = R.string.main_screen_message,
-        modifier = Modifier.clickable {
-            navController.navigate(Destination.ShoppingLists.route)
-        }
-
-    )
-
-}
-
-@Composable
-fun RegisterScreen() {
-
 }
 
 @Composable
